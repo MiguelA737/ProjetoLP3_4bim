99d20618951cae6568888c311c5103d37ebe58b1
Ciziks <luciziks@hotmail.com>
2019-11-12 10:23:23 -0200

    a

--- ProjetoLP3_4bim/ProjetoLP3_4bim/Models/Autor.cs
+++ ProjetoLP3_4bim/ProjetoLP3_4bim/Models/Autor.cs
@@ -17,31 +17,21 @@ namespace ProjetoLP3_4bim.Models
         public int IdAutor { get; set; }
         [Required]
         [Column("nomeAutor")]
-        [Display(Name = "Nome")]
-        [StringLength(60, ErrorMessage = "O nome deve ter no máximo 60 caracteres.")]
+        [StringLength(1)]
         public string NomeAutor { get; set; }
-
-        [DataType(DataType.Date)]
         [Column("dataNascAutor", TypeName = "date")]
         public DateTime DataNascAutor { get; set; }
-
         [Column("nacionalidadeAutor")]
-        [Display(Name = "Nacionalidade")]
-        [StringLength(20, ErrorMessage = "A nacionalidade deve ter no máximo 20 caracteres.")]
+        [StringLength(1)]
         public string NacionalidadeAutor { get; set; }
-
         [Required]
         [Column("telAutor")]
-        [Display(Name = "Telefone")]
-        [StringLength(11, ErrorMessage = "O telefone deve ter no máximo 11 números.")]
+        [StringLength(11)]
         public string TelAutor { get; set; }
-
         [Required]
         [Column("emailAutor")]
-        [Display(Name = "E-mail")]
-        [StringLength(20, ErrorMessage = "O e-mail deve ter no máximo 20 caracteres.")]
+        [StringLength(1)]
         public string EmailAutor { get; set; }
-
 
         [InverseProperty("AutorIdAutorNavigation")]
         public ICollection<Livro> Livro { get; set; }
